ead1a03267402baa864eddcf7c64f6d6aee1fe1f
Dheeraj Bishnoi <141246670+DheerajBishnoi@users.noreply.github.com>
2024-08-15 11:42:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,9 @@
 //create web server
-var express = require('express');
-var app = express();
-
-//create a router
-var router = express.Router();
-
-//create a route
-router.get('/comments', function(req, res) {
-  res.send('Comments');
-});
-
-//mount the router on the app
-app.use('/', router);
-
-//start the server
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
-});
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
