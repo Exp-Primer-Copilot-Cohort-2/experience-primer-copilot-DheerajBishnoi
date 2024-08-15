5a9e7adb83a9503c4c55e5916eec08a550392067
Dheeraj Bishnoi <141246670+DheerajBishnoi@users.noreply.github.com>
2024-08-15 17:14:36 +0530

    Delete comments.js

--- comments.js
+++
@@ -1,9 +0,0 @@
-//create web server
-const express = require('express');
-const app = express();
-const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const port = 3000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
